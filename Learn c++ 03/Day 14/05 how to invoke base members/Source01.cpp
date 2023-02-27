9bb1e069394d83c7a037b7ae8e7b85a8f9c79544
ashu1281 <gaiwadashish628@gmail.com>
2023-02-28 00:19:19 +0530

    invoking base members

--- Learn c++ 03/Day 14/05 how to invoke base members/Source01.cpp
+++ Learn c++ 03/Day 14/05 how to invoke base members/Source01.cpp
@@ -28,6 +28,8 @@ public:
 public:
 	void Print() {
 		Base::Print(); //here m_i is private data member of base class so it is directly not accessible.
+		//cout << m_i << endl;// not possible
+		
 		cout << m_j << endl;
 	}
 private:
